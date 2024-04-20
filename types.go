d58de44123b340b09e496c914d07a32d534ac22a
husky <husky.robot.dog@gmail.com>
2024-04-21 01:05:10 +0800

    coding

--- types.go
+++ types.go
@@ -1,5 +1,11 @@
 package starfish_sdk
 
+import (
+	"database/sql/driver"
+	"errors"
+	"strconv"
+)
+
 type Result[D any] struct {
 	Code ICode
 	Data D
@@ -10,7 +16,37 @@ func (r *Result[D]) IsOk() bool {
 }
 
 func (r *Result[D]) IsFaild() bool {
+
 	return !r.IsOk()
 }
 
 type ID int64
+
+func (v ID) Value() (driver.Value, error) {
+	return int64(v), nil
+}
+
+func (v *ID) Scan(src interface{}) error {
+	if src == nil {
+		return nil
+	}
+	sv, ok := src.(int64)
+	if !ok {
+		return errors.New("类型转换错误")
+	}
+	*v = ID(sv)
+	return nil
+}
+
+func (v ID) MarshalJSON() ([]byte, error) {
+	return []byte(strconv.FormatInt(int64(v), 10)), nil
+}
+
+func (v *ID) UnmarshalJSON(src []byte) error {
+	d, err := strconv.ParseInt(string(src), 10, 64)
+	if err != nil {
+		return err
+	}
+	*v = ID(d)
+	return nil
+}
