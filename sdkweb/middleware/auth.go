010e2c3a1c67aea092f0207c9eafaae13a731f16
husky <husky.robot.dog@gmail.com>
2024-05-01 09:39:27 +0800

    coding

--- sdkweb/middleware/auth.go
+++ sdkweb/middleware/auth.go
@@ -1,8 +1,7 @@
 package sdkwebmiddleware
 
 import (
-	"net/http"
-
+	"github.com/StarfishProgram/starfish-sdk/sdk"
 	"github.com/StarfishProgram/starfish-sdk/sdkauth"
 	"github.com/StarfishProgram/starfish-sdk/sdkcodes"
 	"github.com/StarfishProgram/starfish-sdk/sdkjwt"
@@ -13,26 +12,11 @@ import (
 func Auth(jwt sdkjwt.Jwt, auth *sdkauth.Auth, domain string) func(*gin.Context) {
 	return func(ctx *gin.Context) {
 		token := ctx.Request.Header.Get("Token")
-		if token == "" {
-			ctx.JSON(http.StatusOK, gin.H{
-				"code": sdkcodes.AccessLimited.Code(),
-				"msg":  "token not found",
-				"i18n": sdkcodes.AccessLimited.I18n(),
-				"data": nil,
-			})
-			ctx.Abort()
-			return
-		}
+		sdk.Assert(token != "", sdkcodes.AccessLimited.WithMsg("Token不存在"))
+
 		userClaims, err := jwt.ParseToken(token)
 		if err != nil {
-			ctx.JSON(http.StatusOK, gin.H{
-				"code": sdkcodes.AccessLimited.Code(),
-				"msg":  err.Error(),
-				"i18n": sdkcodes.AccessLimited.I18n(),
-				"data": nil,
-			})
-			ctx.Abort()
-			return
+			sdk.Assert(false, sdkcodes.AccessLimited.WithMsg(err.Error()))
 		}
 		roleId := userClaims.RoleId
 		url := ctx.Request.URL.Path
@@ -41,25 +25,9 @@ func Auth(jwt sdkjwt.Jwt, auth *sdkauth.Auth, domain string) func(*gin.Context) 
 		ok, err := auth.Enforce(roleId.String(), domain, url, method)
 		if err != nil {
 			sdklog.Error(err)
-			ctx.JSON(http.StatusOK, gin.H{
-				"code": sdkcodes.Internal.Code(),
-				"msg":  sdkcodes.Internal.Msg(),
-				"i18n": sdkcodes.Internal.I18n(),
-				"data": nil,
-			})
-			ctx.Abort()
-			return
+			sdk.Assert(false, sdkcodes.Internal)
 		}
-		if !ok {
-			ctx.JSON(http.StatusOK, gin.H{
-				"code": sdkcodes.AccessLimited.Code(),
-				"msg":  sdkcodes.AccessLimited.Msg(),
-				"i18n": sdkcodes.AccessLimited.I18n(),
-				"data": nil,
-			})
-			ctx.Abort()
-			return
-		}
+		sdk.Assert(ok, sdkcodes.AccessLimited)
 
 		if jwt.NeedFlush(userClaims) {
 			newToken, err := jwt.FlushToken(userClaims)
