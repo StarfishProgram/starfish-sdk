df5834e48a5e9b806ab9b1aecac7299be33ba2dc
husky <husky.robot.dog@gmail.com>
2024-05-02 14:26:53 +0800

    coding

--- sdktypes/db.go
+++ sdktypes/db.go
@@ -1,16 +1,14 @@
 package sdktypes
 
 import (
-	"fmt"
-	"slices"
 	"strings"
 	"time"
 )
 
 type BaseModel struct {
-	Id         ID        `gorm:"primarykey;type:bigint;not null;comment:ID;" json:"id"`
-	CreateTime time.Time `gorm:"->;type:timestamp;default:current_timestamp;not null;comment:创建时间;" json:"createTime"`
-	UpdateTime time.Time `gorm:"->;type:timestamp;default:current_timestamp on update current_timestamp;not null;comment:修改时间;" json:"updateTime"`
+	Id         ID        `gorm:"primarykey;type:bigint;not null;comment:ID;" json:"id" form:"id"`
+	CreateTime time.Time `gorm:"->;type:timestamp;default:current_timestamp;not null;comment:创建时间;" json:"createTime" form:"createTime"`
+	UpdateTime time.Time `gorm:"->;type:timestamp;default:current_timestamp on update current_timestamp;not null;comment:修改时间;" json:"updateTime" form:"updateTime"`
 }
 
 func NewBaseModel(id ID) BaseModel {
@@ -22,13 +20,13 @@ func NewBaseModel(id ID) BaseModel {
 }
 
 type PagingSortKey struct {
-	Key string `form:"key" json:"key" binding:"required"`
+	Key string `form:"key" json:"key"`
 	Asc bool   `form:"asc" json:"asc"`
 }
 
 type PagingParam struct {
-	Page  int64           `form:"page" json:"page" binding:"gte=0"`
-	Rows  int64           `form:"rows" json:"rows" binding:"required,gte=1,lte=1000"`
+	Page  int64           `form:"page" json:"page" binding:"gte=0,lte=1000"`
+	Rows  int64           `form:"rows" json:"rows" binding:"required,gte=1,lte=10000"`
 	Sorts []PagingSortKey `form:"sorts" json:"sorts"`
 }
 
@@ -41,25 +39,20 @@ func (p *PagingParam) Limit() int64 {
 	return p.Rows
 }
 
-// CheckSortKey 检查排序的key是否合法
-func (p *PagingParam) CheckSortKey(allowKeys ...string) error {
-	for _, item := range p.Sorts {
-		if !slices.Contains(allowKeys, item.Key) {
-			return fmt.Errorf("invalid sort key : `%s`", item.Key)
-		}
-	}
-	return nil
-}
-
-// SortString 获取排序SQL
-func (p *PagingParam) SortSQLString() *string {
+// SortSQLString 获取排序SQL
+func (p *PagingParam) SortSQLString(rules map[string]string) *string {
 	if len(p.Sorts) == 0 {
 		return nil
 	}
 	var sb strings.Builder
+
 	for i := 0; i < len(p.Sorts); i++ {
 		item := p.Sorts[i]
-		sb.WriteString(item.Key)
+		field, exists := rules[item.Key]
+		if !exists {
+			continue
+		}
+		sb.WriteString(field)
 		if item.Asc {
 			sb.WriteString(" asc")
 		} else {
@@ -68,6 +61,9 @@ func (p *PagingParam) SortSQLString() *string {
 		if len(p.Sorts)-1 != i {
 			sb.WriteString(",")
 		}
+	}
+	if sb.Len() == 0 {
+		return nil
 	}
 	s := sb.String()
 	return &s
