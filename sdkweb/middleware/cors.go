13584dfe003008c5ad7995607b2c1ad1ba5b6a75
husky <husky.robot.dog@gmail.com>
2024-05-03 19:39:34 +0800

    coding

--- sdkweb/middleware/cors.go
+++ sdkweb/middleware/cors.go
@@ -13,6 +13,7 @@ func Cors(ctx *gin.Context) {
 	ctx.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 	ctx.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type, Token, CAPTCHA_ID, CAPTCHA_CODE")
 	ctx.Header("Access-Control-Allow-Credentials", "true")
+	ctx.Header("Access-Control-Max-Age", "86400")
 	if method == "OPTIONS" {
 		ctx.AbortWithStatus(http.StatusNoContent)
 		return
